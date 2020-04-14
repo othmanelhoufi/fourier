c8d96e44428dcb380abb45623478b7df1b9ce943
EL HOUFI othman <othmanjobsblog@gmail.com>
2020-04-14 03:02:45 +0200

    Updates

---
+++ precache-manifest.518622984da2c6f50dd58ef1649185e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3d1814375b9d60301dd647ae224c61",
+    "url": "/fourier/index.html"
+  },
+  {
+    "revision": "5b2b58ac2cc07d08a6f9",
+    "url": "/fourier/static/css/main.b8814931.chunk.css"
+  },
+  {
+    "revision": "87b52c6c847893135485",
+    "url": "/fourier/static/js/2.2cf52672.chunk.js"
+  },
+  {
+    "revision": "20807da88fa5294c04e382222c760935",
+    "url": "/fourier/static/js/2.2cf52672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2b58ac2cc07d08a6f9",
+    "url": "/fourier/static/js/main.366101be.chunk.js"
+  },
+  {
+    "revision": "4c0e3584f7840d8c4c3d",
+    "url": "/fourier/static/js/runtime-main.17154dbb.js"
+  }
+]);
